484ddd02a1234817bb9cd68342bd20bc5c90ed39
Lisong Zhou <zhoulisong@itiger.com>
2019-01-15 13:58:36 +0800

    20190115-135836

---
+++ cron_sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+BOOKMARK_PATH=/Users/zhoulisu/Library/Application Support/Google/Chrome/Default/Bookmarks
+GIT_PATH=/Users/zhoulisu/git/chromeBookmarkSync
+TIME=`date +%Y%m%d-%H%M%S`
+
+cd $GIT_PATH
+cp $BOOKMARK_PATH ./
+git add -A
+git commit -m $TIME
+git push origin master
+
